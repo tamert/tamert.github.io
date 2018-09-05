7e2193414e89590d185b8d675ab1af92c68920c4
Tamer Ağaoğlu <farerock@gmail.com>
2018-09-05 22:43:07 +0300

    güncelleme
    güncelleme

---
+++ js/script.js
@@ -0,0 +1,27 @@
+!function ($){
+
+    /*
+    * Google prettify settings
+    * */
+    $("pre").addClass("prettyprint");
+    prettyPrint();
+
+
+    /*
+    * Drop-down navigation for Mobile devices
+    * */
+    $(function () {
+
+//        $("nav").prepend("<a href='#' class='menu-icon'>navigation</a>");
+
+//        $(".menu-icon").click(function () {
+//            $("nav ul").slideToggle("fast");
+//            return false;
+//        });
+
+    });
+
+}(window.jQuery);
+
+
+
